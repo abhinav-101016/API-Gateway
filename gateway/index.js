ba5471b5049214800f2287042358bec2b7f34252
abhinav101016 <kamla7947@gmail.com>
2026-05-30 20:58:26 +0530

    Added proxy using http-proxy

--- gateway/index.js
+++ gateway/index.js
@@ -1,11 +1,18 @@
-import http from "http";
+import http from "http"
+import httpProxy from 'http-proxy'
+import { routes } from "./config/routes.js"
+const proxy=httpProxy.createProxyServer()
+
 const server=http.createServer((req,res)=>{
-    console.log(req.method,req.url,req.headers)
-    res.writeHead(200,{'content-type':'application/json'})
+    console.log(`[${req.method}] ${req.url}`)
+    const route=routes.find(r=>req.url.startsWith(r.path))
+    if(!route){
+        res.writeHead(404,{"content-type":"application/json"})
+        res.end(JSON.stringify({error:'Route not found'}))
+        return 
+    }
+    proxy.web(req,res,{target:route.target})
 
-
-   
-    res.end(JSON.stringify({message:'hello'}))
 })
 server.listen(3000,()=>{
     console.log("Server running on port 3000")
